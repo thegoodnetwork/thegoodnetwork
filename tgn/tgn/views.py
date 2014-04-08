b676932de17240a2e2f910fb7e40d5026a0caa89
WillHaack <wmh1993@gmail.com>
2014-04-08 17:59:06 -0400

    got the index.html working

--- tgn/tgn/views.py
+++ tgn/tgn/views.py
@@ -1,4 +1,5 @@
-from django.http import render_to_response
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 def test(request):
     return render_to_response("index.html")
