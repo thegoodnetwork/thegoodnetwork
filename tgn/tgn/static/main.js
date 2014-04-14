f9dd1210bf4160c545717018e437cdc920d09e26
WillHaack <wmh1993@gmail.com>
2014-04-14 19:42:21 -0400

    made a few changes

--- tgn/tgn/static/main.js
+++ tgn/tgn/static/main.js
@@ -9,3 +9,13 @@ angularTGN.controller('myProfileController', function($scope) {
     $scope.skills = "programmer, dancer, runner"
     $scope.affiliations = "The Good Network, CSTUY"
 });
+
+angularTGN.controller('otherProfileController', function($scope) {
+    $scope.profileLink =
+	"https://scontent-b.xx.fbcdn.net/hphotos-prn2/t1.0-9/1780688_10151958313376483_441748523_n.jpg"
+    $scope.name = "Demitri Nava";
+    $scope.aboutMe = "I am a half-decent programmer as evident by the quality of the site you are currently looking at. This is some text that lets the page look a little more even. Without this text, the left side of the page would look like it had a lot more content than this side of the page. Some say it still looks like that even with all this text. ";
+    $scope.resumeLink = "www.yahoo.com";
+    $scope.skills = "surfer, singer, writer";
+    $scope.affiliations = "The Farm Co, Sean's Outpost";   
+});
