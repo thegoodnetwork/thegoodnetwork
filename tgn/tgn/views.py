d3fd70340792ae47ead343d7ca733a465f984404
demitri <demitri@mit.edu>
2014-04-15 21:03:44 -0400

    fixed views, tests werent ruined with new additions

--- tgn/tgn/views.py
+++ tgn/tgn/views.py
@@ -9,36 +9,25 @@ def fbtest(request):
     return render_to_response('fbloginindex.html')
 
 
-def formattedResponse(isError=False, errorMessage=None, data=None):
-    '''
-    Returns a properly formatted response for our API.
-    :param isError: Whether or not there was an error.
-    :param errorMessage: A human readable error message.
-    :param data: The appropriate data if the response
-    '''
-
-    response = {
-        'isError': isError,
-        'errorMessage': errorMessage,
-        'data': data,
-    }
-
-    return HttpResponse(json.dumps(response), content_type="application/json")
-
 def profile(request):
-    return render_to_response('profile.html');
+    return render_to_response('profile.html')
+
 
 def otherProfile(request):
-    return render_to_response('otherProfile.html');
+    return render_to_response('otherProfile.html')
+
 
 def otherNonprofit(request):
-    return render_to_response('otherNonprofit.html');
+    return render_to_response('otherNonprofit.html')
+
 
 def myNonprofit(request):
-    return render_to_response('myNonprofit.html');
+    return render_to_response('myNonprofit.html')
+
 
 def myJob(request):
-    return render_to_response('myJob.html');
+    return render_to_response('myJob.html')
+
 
 def searchResults(request):
-    return render_to_response('searchResults.html');
+    return render_to_response('searchResults.html')
