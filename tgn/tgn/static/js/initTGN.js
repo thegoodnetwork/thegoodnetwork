0fb68b2eb2150554812fedcfcc3e888f82228767
demitri <demitri@mit.edu>
2014-05-07 19:17:37 -0400

    trying to get this working

--- tgn/tgn/static/js/initTGN.js
+++ tgn/tgn/static/js/initTGN.js
@@ -1,4 +1,4 @@
-var tgn = angular.module('angularTGN', [], function ($httpProvider) {
+var tgn = angular.module('app', ['ngRoute'], function ($httpProvider) {
     // Use x-www-form-urlencoded Content-Type
     $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset\
 =utf-8';
